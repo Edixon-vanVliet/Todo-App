7b0f12630b5f849940d912a77e6d4df87f608d69
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-07-25 12:57:28 -0400

    Connect to database

--- back-end/app.js
+++ back-end/app.js
@@ -2,11 +2,24 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+require("dotenv/config");
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+mongoose.connect(
+    process.env.DB_Connection,
+    // to avoid deprecation warnings as indicated at
+    // https://mongoosejs.com/docs/deprecations.html
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    () => {
+        console.log("Connected to DB!");
+    }
+);
 
 module.exports = app;
