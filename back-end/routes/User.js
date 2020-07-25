7ceb3bac2747ba8aeeeb5b2fe235a27619705378
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-07-25 16:13:03 -0400

    Create route for users

---
+++ back-end/routes/User.js
@@ -0,0 +1,15 @@
+const Router = require("express").Router();
+
+const upload = require("../controllers/UploadImage");
+const validation = require("../validators");
+const { validateCreateUserFields } = require("../validators/users/createUsers");
+const { addUser } = require("../controllers/User");
+
+Router.post(
+    "/",
+    upload.single("photo"),
+    validation(validateCreateUserFields),
+    addUser
+);
+
+module.exports = Router;
