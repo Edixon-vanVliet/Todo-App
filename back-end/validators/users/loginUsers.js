Edixon-vanVliet/Todo-App

commit a304a622d09021386503d07209c42b1db1a6b5d3

Validate password

back-end/validators/users/loginUsers.js
@@ -1,9 +1,20 @@
 const { body } = require("express-validator");
 const User = require("../../models/User.model");
+const { compare } = require("bcryptjs");
+
+let password = "";
 
 const exists = async (value, { path }) => {
     const user = await User.findOne({ [path]: value });
     if (!user) {
+        return Promise.reject("Incorrect credentials");
+    }
+    password = user.password;
+};
+
+const validatePassword = async (value) => {
+    const validPassword = await compare(value, password);
+    if (!validPassword) {
         return Promise.reject("Incorrect credentials");
     }
 };
@@ -26,5 +37,6 @@ exports.validateLoginUserFields = [
         .isAscii()
         .withMessage("Invalid characters")
         .isLength({ min: 8, max: 25 })
-        .withMessage("Password should have between 6 and 25 characters."),
+        .withMessage("Password should have between 8 and 25 characters.")
+        .custom(validatePassword),
 ];

commit fc40ecdd8b64b3dccc49d69ccb31ce89dfd00fb4

Create function authenticateUser
function was created to avoid async bug caused by commit a304a62

back-end/validators/users/loginUsers.js
@@ -2,18 +2,15 @@ const { body } = require("express-validator");
 const User = require("../../models/User.model");
 const { compare } = require("bcryptjs");
 
-let password = "";
+const authenticateUser = async (value, { req }) => {
+    const { userName } = req.body;
 
-const exists = async (value, { path }) => {
-    const user = await User.findOne({ [path]: value });
+    const user = await User.findOne({ userName });
     if (!user) {
         return Promise.reject("Incorrect credentials");
     }
-    password = user.password;
-};
 
-const validatePassword = async (value) => {
-    const validPassword = await compare(value, password);
+    const validPassword = await compare(value, user.password);
     if (!validPassword) {
         return Promise.reject("Incorrect credentials");
     }
@@ -28,8 +25,7 @@ exports.validateLoginUserFields = [
         .withMessage("Invalid characters")
         .isLength({ min: 6 })
         .withMessage("Username should have minimum 6 characters.")
-        .trim()
-        .custom(exists),
+        .trim(),
     body("password")
         .not()
         .isEmpty()
@@ -38,5 +34,5 @@ exports.validateLoginUserFields = [
         .withMessage("Invalid characters")
         .isLength({ min: 8, max: 25 })
         .withMessage("Password should have between 8 and 25 characters.")
-        .custom(validatePassword),
+        .custom(authenticateUser),
 ];